c3a598249807137a0484d866f7569fb238bc0f45
Toshiaki Maki <tmaki@vmware.com>
2023-06-21 19:36:32 +0900

    more binary like format

--- src/main/java/am/ik/bf/codegen/WatCodeGenerator.java
+++ src/main/java/am/ik/bf/codegen/WatCodeGenerator.java
@@ -29,18 +29,22 @@ public class WatCodeGenerator extends PrintStreamCodeGenerator {
 	public void begin() {
 		this.out.print("""
 				(module
-				    (import "wasi_snapshot_preview1" "fd_write" (func $fd_write (param i32 i32 i32 i32) (result i32)))
-				    (import "wasi_snapshot_preview1" "fd_read"  (func $fd_read  (param i32 i32 i32 i32) (result i32)))
-				    (memory (export "memory") 1)
-				    (global $pointer (mut i32) (i32.const 128))
-				    (func (export "_start")
+				  (type (;0;) (func (param i32 i32 i32 i32) (result i32)))
+				  (type (;1;) (func))
+				  (import "wasi_snapshot_preview1" "fd_write" (func (;0;) (type 0)))
+				  (import "wasi_snapshot_preview1" "fd_read" (func (;1;) (type 0)))
+				  (func (;2;) (type 1)
 				""");
 	}
 
 	@Override
 	public void end() {
 		this.out.print("""
-				    )
+				  )
+				  (memory (;0;) 1)
+				  (global (;0;) (mut i32) (i32.const 128))
+				  (export "memory" (memory 0))
+				  (export "_start" (func 2))
 				)""");
 
 	}
@@ -48,84 +52,84 @@ public class WatCodeGenerator extends PrintStreamCodeGenerator {
 	@Override
 	public void generateLoopStatement(LoopStatement statement) {
 		this.out.printf("""
-				        loop ;; @%d
-				        block ;; @%d
-				        global.get $pointer
-				        i32.load8_u
-				        i32.eqz
-				        br_if 0 ;; @%d
+				    loop ;; @%d
+				    block ;; @%d
+				    global.get 0
+				    i32.load8_u
+				    i32.eqz
+				    br_if 0 ;; @%d
 				""", this.loopDepth * 2 + 1, this.loopDepth * 2 + 2, this.loopDepth * 2 + 2);
 		statement.statements().forEach(s -> s.generate(this.inLoop()));
 		this.out.printf("""
-				        br 1 ;; @%d
-				        end ;; @%d
-				        end ;; @%d
+				    br 1 ;; @%d
+				    end ;; @%d
+				    end ;; @%d
 				""", this.loopDepth * 2 + 1, this.loopDepth * 2 + 2, this.loopDepth * 2 + 1);
 	}
 
 	@Override
 	public void generateOutputStatement(OutputStatement statement) {
 		this.out.print("""
-				        ;; .
-				        ;; iov
-				        i32.const 0
-				        global.get $pointer
-				        i32.store
-				        i32.const 4
-				        i32.const 1 ;; 1 byte
-				        i32.store
-				        ;; print
-				        i32.const 1 ;; stdout
-				        i32.const 0 ;; iov index
-				        i32.const 1 ;; iov len
-				        i32.const 20
-				        call $fd_write
-				        drop
+				    ;; .
+				    ;; iov
+				    i32.const 0
+				    global.get 0
+				    i32.store
+				    i32.const 4
+				    i32.const 1 ;; 1 byte
+				    i32.store
+				    ;; print
+				    i32.const 1 ;; stdout
+				    i32.const 0 ;; iov index
+				    i32.const 1 ;; iov len
+				    i32.const 20
+				    call 0
+				    drop
 				""");
 	}
 
 	@Override
 	public void generateInputStatement(InputStatement statement) {
 		this.out.print("""
-				        ;; ,
-				        ;; iov
-				        i32.const 0
-				        global.get $pointer
-				        i32.store
-				        i32.const 4
-				        i32.const 1 ;; 1 byte
-				        i32.store
-				        ;; read
-				        i32.const 0 ;; stdin
-				        i32.const 0 ;; iov index
-				        i32.const 1 ;; iov len
-				        i32.const 20
-				        call $fd_read
-				        drop
+				    ;; ,
+				    ;; iov
+				    i32.const 0
+				    global.get 0
+				    i32.store
+				    i32.const 4
+				    i32.const 1 ;; 1 byte
+				    i32.store
+				    ;; read
+				    i32.const 0 ;; stdin
+				    i32.const 0 ;; iov index
+				    i32.const 1 ;; iov len
+				    i32.const 20
+				    call 1
+				    drop
 				""");
 	}
 
 	@Override
 	public void generateIncrementValueExpression(IncrementValueExpression expression) {
 		this.out.printf("""
-				        ;; %s
-				        global.get $pointer
-				        global.get $pointer
-				        i32.load8_u
-				        i32.const 1
-				        i32.%s
-				        i32.store8
+				    ;; %s
+				    global.get 0
+				    global.get 0
+				    i32.load8_u
+				    i32.const 1
+				    i32.%s
+				    i32.store8
 				""", expression.value() > 0 ? "+" : "-", expression.value() > 0 ? "add" : "sub");
 	}
 
 	@Override
 	public void generateIncrementPointerExpression(IncrementPointerExpression expression) {
 		this.out.printf("""
-				        ;; %s
-				        global.get $pointer
-				        i32.const 1
-				        i32.%s
-				        global.set $pointer
+				    ;; %s
+				    global.get 0
+				    i32.const 1
+				    i32.%s
+				    global.set 0
 				""", expression.value() > 0 ? ">" : "<", expression.value() > 0 ? "add" : "sub");
 	}
 
